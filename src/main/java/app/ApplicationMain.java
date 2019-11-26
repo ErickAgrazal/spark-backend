0d94d16628855f3b1f1fd3741c00d00bd0e9581c
Ercik Agrazal <erick.agrazal@balboadevelopers.com>
2019-11-25 19:17:25 -0500

    Added cors.

--- src/main/java/app/ApplicationMain.java
+++ src/main/java/app/ApplicationMain.java
@@ -7,12 +7,32 @@ import app.Group;
 public class ApplicationMain {
   public static void main(String[] args) {
     // Initialize basic model
+    enableCors();
     Group group = createGroup();
     get("/", (req, res) -> {
       res.type("application/json");
       JSONObject jo = new JSONObject(group);
       return jo;
     });
+  }
+
+  public static void enableCors() {
+    options("/*", (request, response) -> {
+
+      String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+      if (accessControlRequestHeaders != null) {
+        response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+      }
+
+      String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+      if (accessControlRequestMethod != null) {
+        response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+      }
+
+      return "OK";
+    });
+
+    before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
   }
 
   public static Group createGroup() {
