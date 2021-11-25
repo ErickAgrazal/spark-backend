4a925c18035bb5931df4e5fb5adeb27dd79f3462
Erick Agrazal <erick.agrazal@gmail.com>
2021-11-24 19:28:37 -0500

    Added grades to students on endpoint.

--- src/main/java/app/ApplicationMain.java
+++ src/main/java/app/ApplicationMain.java
@@ -6,14 +6,18 @@ import static spark.Spark.options;
 import static spark.Spark.post;
 
 import java.util.List;
+
 import java.util.ArrayList;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.BasicConfigurator;
 
 public class ApplicationMain {
+  static Logger logger = Logger.getLogger(ApplicationMain.class);
+
   public static void main(String[] args) {
     // ==========================================
     get("/hola", (request, response) -> {
@@ -34,12 +38,33 @@ public class ApplicationMain {
       return request.body();
     });
     // ==========================================
+    BasicConfigurator.configure();
     Group group = createGroup();
     get("/estudiantes", (req, res) -> {
       res.type("application/json");
       JSONObject jo = new JSONObject(group);
       return jo;
     });
+    post("/estudiantes", (req, res) -> {
+      res.type("application/json");
+      JSONObject body = new JSONObject(req.body());
+      Student student = new Student();
+      if(!body.has("name") && !body.has("age")){
+          return "Invalid request";
+      }
+      student.setName(body.getString("name"));
+      student.setAge(body.getInt("age"));
+      student.setGrades(getGradesFromJSONArray(body.getJSONArray("grades")));
+      group.addStudent(student);
+      JSONObject jo = new JSONObject(group);
+      return jo;
+    });
+  }
+
+  public static List<Integer> getGradesFromJSONArray(JSONArray _grades){
+    List<Integer> newGrades = new ArrayList<Integer>();
+    for(int i = 0; i < _grades.length(); i++){ newGrades.add(_grades.getInt(i)); }
+    return newGrades;
   }
 
   public static Group createGroup(){
